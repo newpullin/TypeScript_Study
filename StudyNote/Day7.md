5106ff807d6c0a674a8c8ba490d46a5b6706d786
newpullin <newpullin@naver.com>
2020-10-28 03:18:01 +0900

    mongo db

--- StudyNote/Day7.md
+++ StudyNote/Day7.md
@@ -5,3 +5,30 @@
 * change 패키지를 사용해 그럴듯한 가짜 데이터 생성 코드 구현
 * CSV 파일 포맷 데이터를 읽고 쓰는 코드 구현
 
+
+## 몽고 DB에 데이터 저장하기
+
+* 몽고DB란?
+
+- 몽고DB는 NoSQL 시스템으로, SQL을 사용하지 않고, 다른 방식의 질의어를 사용합니다.
+
+* 몽고DB 설치
+
+(
+    scoop 설치 방법, 관리자 권한으로 윈도우 파워셸 실행
+    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+    $env:SCOOP='C:\Scoop'
+    iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+    scoop install aria2
+    scoop install git
+)
+
+scoop install mongodb
+mongod --install
+net start mongodb
+
+
+* 몽고DB vs 관계형 DB
+
+   문서   -  레코드
+  컬렉션  -  테이블
---
+++ ch12-1/src/test/delete-test.ts
@@ -0,0 +1,26 @@
+import {connect} from '../mongodb/connect'
+
+const deleteTest = async() => {
+    let connection
+    try {
+        connection = await connect()
+        const db = await connection.db('ch12-2')
+        const personsCollection = db.collection('persons')
+        const addressesCollection = db.collection('addresses')
+        await personsCollection.insertMany([
+            {name: 'Jack'}, {name: 'Tom'}, {name: 'Jane'}
+        ])
+
+        let result = await personsCollection.deleteOne({name: 'Tom'})
+        console.log(result)
+
+        result = await personsCollection.deleteMany({})
+        console.log(result)
+    }catch(e) {
+        console.log(e.message)
+    }finally{
+        connection.close()
+    }
+}
+
+deleteTest()
